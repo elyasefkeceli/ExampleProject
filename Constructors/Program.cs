7021f738d89e15aebce0c99ba2c4efb35b136b07
elyase faruk keçeli <elyasefkeceli@gmail.com>
2021-03-23 22:42:51 +0300

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName="elyase",LastName="keceli",City="Ordu" };
+            Customer customer2 = new Customer(2,"faruk","keceli","ankara");
+            Console.WriteLine(customer2.FirstName);
+            Customer customer3 = new Customer();
+            customer3.Id = 4;
+            customer3.FirstName = "Furkan";
+            customer3.LastName = "keceli";
+            customer3.City = "trabzon";
+
+            //Method(3,"fatih","keceli","samsun");
+        }
+        //static void Method(int id,string firstName,string lastName,string city)
+        //{
+
+        //}
+    }
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        //default constructor
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ ReferenceTypes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+            person1.FirstName = "Elyase";
+
+            person1 = person2;
+            person1.FirstName = "Derin";
+            Console.WriteLine(person2.FirstName);
+            Customer customer = new Customer();
+            customer.FirstName = "Salih";
+            customer.CreditCardNumber = "12345543";
+            Employee employee = new Employee();
+            employee.FirstName = "Betül";
+            Person person3 = customer;
+            customer.FirstName = "Ahmet";
+            //Console.WriteLine(((Customer)person3).CreditCardNumber);
+            //Console.WriteLine(person3.FirstName);
+            PersonManager personManager = new PersonManager();
+            personManager.Add(employee);
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    //base class : Person
+    class Customer : Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+    class Employee : Person
+    {
+        public string EmployeeNumber { get; set; }
+
+    }
+    class PersonManager
+    {
+        public void Add(Person person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
